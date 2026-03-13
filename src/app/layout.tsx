ca635906ed934381e356a7814e3f33540a53bb95
Becca Cardwell <42225076+beccatcardwell@users.noreply.github.com>
2026-03-12 21:21:08 -0700

    Update packages and add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,46 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "@/styles/globals.css"
-import StoreProvider from "@/components/StoreProvider"
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+import StoreProvider from "@/components/StoreProvider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://next-redux-boilerplate.vercel.app'),
+  metadataBase: new URL("https://next-redux-boilerplate.vercel.app"),
   title: "Next Redux Boilerplate",
-  description: "A boilerplate for Next.js and Redux using App Router, @reduxjs/toolkit, and react-redux.",
-  keywords: ['Next.js', 'React', 'Redux', 'TypeScript', '@reduxjs/toolkit', 'react-redux', 'app router'],
-  creator: 'Becca Cardwell',
+  description:
+    "A boilerplate for Next.js and Redux using App Router, @reduxjs/toolkit, and react-redux.",
+  keywords: [
+    "Next.js",
+    "React",
+    "Redux",
+    "TypeScript",
+    "@reduxjs/toolkit",
+    "react-redux",
+    "app router",
+  ],
+  creator: "Becca Cardwell",
   openGraph: {
-    title: 'Next Redux Boilerplate',
-    type: 'website',
-    description: "A boilerplate for Next.js and Redux using App Router, @reduxjs/toolkit, and react-redux.",
-    siteName: 'Next.js',
-    url: '/',
-    locale: 'en_US',
+    title: "Next Redux Boilerplate",
+    type: "website",
+    description:
+      "A boilerplate for Next.js and Redux using App Router, @reduxjs/toolkit, and react-redux.",
+    siteName: "Next.js",
+    url: "/",
+    locale: "en_US",
     images: {
-      url: '/images/screenshot.png',
-      alt: 'Next.js Redux Boilerplate homepage'
+      url: "/images/screenshot.png",
+      alt: "Next.js Redux Boilerplate homepage",
     },
   },
   twitter: {
-    title: 'Next Redux Boilerplate',
-    description: "A boilerplate for Next.js and Redux using App Router, @reduxjs/toolkit, and react-redux.",
-    creator: '@beccatcardwell',
-    images: '/images/screenshot.png'
-  }
-}
+    title: "Next Redux Boilerplate",
+    description:
+      "A boilerplate for Next.js and Redux using App Router, @reduxjs/toolkit, and react-redux.",
+    creator: "@beccatcardwell",
+    images: "/images/screenshot.png",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -42,5 +53,5 @@ export default function RootLayout({
         <StoreProvider>{children}</StoreProvider>
       </body>
     </html>
-  )
+  );
 }
